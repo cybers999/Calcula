4fe246f3f0ddbed265ec426d6b0c46686381d34e
Hiba <anas.wakif2000@gmail.com>
2021-01-18 19:10:16 +0100

    adding functions

--- calcul.py
+++ calcul.py
@@ -5,4 +5,7 @@ def soustraction(a,b):
 def multiplication(a,b):
     return a * b
 def division(a,b):
-    return a / b
+    if b != 0
+        return a/b
+    else :
+        return "Opération Impossible.."
