72ed9d1190f9dc602ffd52d05e09da3ff0b42a91
Hiba <anas.wakif2000@gmail.com>
2021-01-18 19:17:14 +0100

    adding functions

--- calcul.py
+++ calcul.py
@@ -1,11 +1,14 @@
 def addition(a,b):
     return a + b
+
 def soustraction(a,b):
     return a - b
+
 def multiplication(a,b):
     return a * b
+
 def division(a,b):
-    if b != 0
+    if b != 0 :
         return a/b
     else :
         return "Opération Impossible.."
