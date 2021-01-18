e0919e82daf2ab0d1fb015aac5c17f1ad1bc9a22
Hiba <anas.wakif2000@gmail.com>
2021-01-18 20:24:49 +0100

    adding functions

--- calcul.py
+++ calcul.py
@@ -1,9 +1,21 @@
+
+
+
 def addition(a,b):
     return a + b
+
+
+
 def soustraction(a,b):
     return a - b
+
+
+
 def multiplication(a,b):
     return a * b
+
+
+
 def division(a,b):
     return a / b
 
